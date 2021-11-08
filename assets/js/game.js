4a6efb78b7ea9aca122e0b413a4770bb57c203cd
Keaton <az.keaton@gmail.com>
2021-11-07 19:58:55 -0700

    concatenated java script

---
+++ assets/js/game.js
@@ -0,0 +1,16 @@
+// this creates a function named "fight"
+
+function fight () {
+    window.alert("the fight has begun!");
+}
+// fight ();
+
+var playerName = window.prompt("What is your robot's name?");
+// What is this?
+console.log(playerName);
+
+console.log("this logs a string, good for leaving yourself a message");
+//this will do math and log 20
+console.log(10+10)
+//what is this?
+console.log("Our robot's name is " + playerName)
